0520ae1aec2651c9b0c643a951bbbea01070a057
unknown <fshz1382@gmail.com>
2025-03-13 12:37:00 +0330

    updated feature

--- quadraticEquation.cpp
+++ quadraticEquation.cpp
@@ -11,7 +11,8 @@ int main()
 
     if (0 == a)
     {
-        std::cout<<"the value of 'a' cannot be set to zero. in that case the equation will be linear.";
+        std::cout<<"the value of 'a' cannot be set to zero. in that case the equation will be linear.\n";
+        system("pause");
         return 0;
     }
 
@@ -22,15 +23,15 @@ int main()
         float x1,x2;
         x1 = ((-b) + sqrt(delta)) / (2*a);
         x2 = ((-b) - sqrt(delta)) / (2*a);
-        std::cout<<"there are two real roots for this quadratic equation."<<"\n\tx1 is: "<<x1<<"\n\tx2 is: "<<x2;
+        std::cout<<"there are two real roots for this quadratic equation."<<"\n\tx1 is: "<<x1<<"\n\tx2 is: "<<x2<<"\n";
     }
 
     else if (delta < 0)
-        std::cout<<"delta is " <<delta<< " .there are no real roots for this equation";
+        std::cout<<"delta is " <<delta<< " .there are no real roots for this equation\n";
     else
     {
         float doubleRoot = (-b) / (2 * a);
-        std::cout<<"delta is '" << delta << "'. in this case there is one real root, called the double root, and is equal to: ("<<doubleRoot<<")";
+        std::cout<<"delta is '" << delta << "'. in this case there is one real root, called the double root, and is equal to: ("<<doubleRoot<<")\n";
     }
-    system("\npause");
+    system("pause");
 }
